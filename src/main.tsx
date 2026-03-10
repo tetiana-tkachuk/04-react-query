8316c26ddd4b872846a52f80a80a293d2f32b3a1
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-03-10 20:34:37 +0200

    Set QueryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './components/App/App.tsx';
 
 import 'modern-normalize';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>
+  </QueryClientProvider>
 );
