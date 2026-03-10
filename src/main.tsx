eb0b8c42bf600dfa793cbf17d90a0988bfddbe2c
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-03-10 20:43:39 +0200

    Set ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './components/App/App.tsx';
 
 import 'modern-normalize';
@@ -9,5 +10,6 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
