16a24b413d7aacbaa2f5eecd134c836f787ab4a6
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2026-03-11 15:47:02 +0200

    Fix API request

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -11,7 +11,7 @@ interface MoviesHttpResponse {
 
 export const fetchMovies = async (query: string, page: number) => {
   const response = await axios.get<MoviesHttpResponse>(
-    `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&${page}`,
+    `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=${page}`,
     {
       params: {},
       headers: {
